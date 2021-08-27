8efffffc21c65f129c91d36f137ec07a8b0a55d2
kevinsuryajaya <kevinsuryajaya08@gmail.com>
2021-08-28 00:30:12 +0700

    commit component

---
+++ src/component/navbar/navbar.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import '../../styles/navbar.scss';
+import companyLogo from '../../assets/stockbit.png';
+import {Link} from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <div className=''>
+            <ul>
+                <li><img className='company-logo' src={companyLogo} alt='company-logo'/></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/watchlist'>Watchlist</Link></li>
+            </ul>
+        </div>
+    )
+}
